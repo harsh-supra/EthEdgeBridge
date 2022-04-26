31cf18fddc38c973946e714f3f7bc0c78a5fece8
Harsh Mashru <92779141+harsh-supra@users.noreply.github.com>
2022-04-26 18:49:20 +0530

    Initial Commit

---
+++ contracts/Bridge.sol
@@ -0,0 +1,99 @@
+pragma solidity ^0.5.0;
+
+import "./EdgewareERC20.sol";
+import "./ValidatorSet.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+contract Bridge is ValidatorSet {
+
+    mapping (string => address) edgewareTokens;
+    mapping (address => bool) edgewareTokenAddresses;
+
+    /* Events  */
+    event NewEdgewareERC20(string name, address tokenAddress);
+    event Lock(bytes to, address token, uint64 value);
+    event Unlock(address to, address token, uint64 value);
+
+    constructor(address[] memory initAddress, uint64[] memory initPowers)
+        public
+        ValidatorSet(initAddress, initPowers) {
+    }
+
+    /* Functions */
+
+    function hashNewEdgewareERC20(string memory name, uint decimals) public pure returns (bytes32 hash) {
+      return keccak256(abi.encodePacked(name, decimals));
+    }
+
+    function hashUnlock(address to, address token, uint64 amount) public pure returns (bytes32 hash) {
+      return keccak256(abi.encodePacked(to, token, amount));
+    }
+
+    function getEdgewareTokenAddress(string memory name) public view returns (address addr) {
+      return edgewareTokens[name];
+    }
+
+
+    function isEdgewareTokenAddress(address addr) public view returns (bool isCosmosAddr) {
+      return edgewareTokenAddresses[addr];
+    }
+
+    /**
+     * @dev             Locks received funds to the consensus of the peg zone
+     * @param to        bytes representation of destination address
+     * @param amount    value of transference
+     * @param tokenAddr token address in origin chain (0x0 if Ethereum, Cosmos for other values)
+     */
+    function lock(bytes memory to, address tokenAddr, uint64 amount) public payable returns (bool) {
+        if (msg.value != 0) {
+          require(tokenAddr == address(0), "Can only send currency to the zero token");
+          require(msg.value == amount, "Invalid lock amount");
+        } else if (edgewareTokenAddresses[tokenAddr]) {
+          EdgewareERC20(tokenAddr).burn(msg.sender, amount);
+        } else {
+          require(ERC20(tokenAddr).transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        }
+        emit Lock(to, tokenAddr, amount);
+        return true;
+    }
+
+    // Unlocks Ethereum tokens according to the information from the pegzone. Called by the relayers.
+    /**
+     * @param to          bytes representation of destination address
+     * @param amount      value of transference
+     * @param token       token address in origin chain (0x0 if Ethereum, Cosmos for other values)
+     * @ param chain      bytes respresentation of the destination chain (not used in MVP, for incentivization of relayers)
+     * @param signers     indexes of each validator
+     * @param v           array of recoverys id
+     * @param r           array of outputs of ECDSA signature
+     * @param s           array of outputs of ECDSA signature
+     */
+    function unlock(address payable to, address token, uint64 amount, uint[] calldata signers, uint8[] calldata v, bytes32[] calldata r, bytes32[] calldata s) external returns (bool) {
+        bytes32 hashData = keccak256(abi.encodePacked(to, token, amount));
+        require(ValidatorSet.verifyValidators(hashData, signers, v, r, s), "Validator verification failed");
+        if (token == address(0)) {
+          to.transfer(amount);
+        } else if (edgewareTokenAddresses[token]) {
+          EdgewareERC20(token).mint(to, amount);
+        } else {
+          require(ERC20(token).transfer(to, amount), "ERC token transfer failed");
+        }
+        emit Unlock(to, token, amount);
+        return true;
+    }
+
+    function newEdgewareERC20(string calldata name, uint decimals, uint[] calldata signers, uint8[] calldata v, bytes32[] calldata r, bytes32[] calldata s) external returns (address addr) {
+        require(edgewareTokens[name] == address(0), "Edgeware ERC20 token with this name already exists");
+
+        bytes32 hashData = keccak256(abi.encodePacked(name, decimals));
+        require(ValidatorSet.verifyValidators(hashData, signers, v, r, s), "Validator verification failed");
+
+        EdgewareERC20 newToken = new EdgewareERC20();
+
+        edgewareTokens[name] = address(newToken);
+        edgewareTokenAddresses[address(newToken)] = true;
+
+        emit NewEdgewareERC20(name, address(newToken));
+        return address(newToken);
+    }
+}
---
+++ contracts/ValidatorSet.sol
@@ -0,0 +1,91 @@
+pragma solidity ^0.5.0;
+
+contract ValidatorSet {
+
+    /* Variables */
+
+    address[] public addresses;
+    uint64[] public powers;
+    uint64 public totalPower;
+    uint internal updateSeq = 0;
+
+
+    /* Events */
+
+    event Update(address[] newAddresses, uint64[] newPowers, uint indexed seq);
+
+
+    /* Getters (These are supposed to be auto implemented by solidity but aren't ¯\_(ツ)_/¯) */
+
+    function getAddresses() public view returns (address[] memory) {
+        return addresses;
+    }
+
+    function getPowers() public view returns (uint64[] memory) {
+        return powers;
+    }
+
+    function getTotalPower() public view returns (uint64) {
+        return totalPower;
+    }
+
+
+    /* Functions */
+
+    function hashValidatorArrays(address[] memory addressesArr, uint64[] memory powersArr) public pure returns (bytes32 hash) {
+        return keccak256(abi.encodePacked(addressesArr, powersArr));
+    }
+
+    function verifyValidators(bytes32 hash, uint[] memory signers, uint8[] memory v, bytes32[] memory r, bytes32[] memory s)
+        public view returns (bool) {
+        uint64 signedPower = 0;
+        for (uint i = 0; i < signers.length; i++) {
+            if (i > 0) {
+                require(signers[i] > signers[i-1], "Signers array not sorted");
+            }
+            address recAddr = ecrecover(hash, v[i], r[i], s[i]);
+            require(recAddr == addresses[signers[i]], "Invalid recipient address");
+
+            signedPower += powers[signers[i]];
+        }
+        require(signedPower * 3 > totalPower * 2, "Not enough signatures");
+        return true;
+    }
+
+
+    function updateInternal(address[] memory newAddress, uint64[] memory newPowers) internal returns (bool) {
+        addresses = new address[](newAddress.length);
+        powers    = new uint64[](newPowers.length);
+        totalPower = 0;
+        for (uint i = 0; i < newAddress.length; i++) {
+            addresses[i] = newAddress[i];
+            powers[i]    = newPowers[i];
+            totalPower  += newPowers[i];
+        }
+        uint updateCount = updateSeq;
+        emit Update(addresses, powers, updateCount);
+        updateSeq++;
+        return true;
+    }
+
+
+    /**
+     * @dev Updates validator set. Called by the relayers.
+     * @param newAddress  new validators addresses
+     * @param newPowers    power of each validator
+     * @param signers     indexes of each signer validator
+     * @param v           recovery id. Used to compute ecrecover
+     * @param r           output of ECDSA signature. Used to compute ecrecover
+     * @param s           output of ECDSA signature.  Used to compute ecrecover
+     */
+    function update(address[] memory newAddress, uint64[] memory newPowers, uint[] memory signers, uint8[] memory v, bytes32[] memory r,
+        bytes32[] memory s) public {
+        bytes32 hashData = keccak256(abi.encodePacked(newAddress, newPowers));
+        require(verifyValidators(hashData, signers, v, r, s), "Invalid validator"); // hashing can be changed
+        require(updateInternal(newAddress, newPowers), "Internal update failed");
+    }
+
+    constructor(address[] memory initAddress, uint64[] memory initPowers) public {
+        updateInternal(initAddress, initPowers);
+    }
+}
